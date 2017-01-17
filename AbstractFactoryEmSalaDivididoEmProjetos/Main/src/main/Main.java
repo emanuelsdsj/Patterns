14dd594f15a57ffc7517a1124eb9a03cbdb78cfe
emanuelssj <emanuelolz@yahoo.com.br>
2017-01-17 16:27:54 -0300

    Adicionado padrão dividido em projetos

---
+++ AbstractFactoryEmSalaDivididoEmProjetos/Main/src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import fabricaCarroLuxo.FabricaCarroLuxo;
+import fabricaCarroPopular.FabricaCarroPopular;
+import interfaces.IAbstractFactory;
+
+/**
+ *
+ * @author aluno
+ */
+public class Main {
+
+    public void usarFabrica(IAbstractFactory factory){
+        factory.createMotor().ligar();
+        factory.createRoda().alinhar();
+        factory.createSom().tocar();
+    }
+    
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.usarFabrica(new FabricaCarroPopular());
+        main.usarFabrica(new FabricaCarroLuxo());
+    }
+    
+}
