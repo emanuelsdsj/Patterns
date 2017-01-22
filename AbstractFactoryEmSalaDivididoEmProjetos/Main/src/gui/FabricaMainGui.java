b9a7b65ba9f832d3342d3b1b65c89d427eb52f03
Emanuel <emanuelolz@yahoo.com.br>
2017-01-22 14:04:54 -0300

    Melhorada a aparência

--- AbstractFactoryEmSalaDivididoEmProjetos/Main/src/gui/FabricaMainGui.java
+++ AbstractFactoryEmSalaDivididoEmProjetos/Main/src/gui/FabricaMainGui.java
@@ -16,6 +16,26 @@ public class FabricaMainGui extends javax.swing.JFrame {
         this.refreshPlugins();
     }
     
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FabricaMainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FabricaMainGui().setVisible(true);
+            }
+        });
+    }
+    
     private void isCarroPronto(){
         if(!this.ligarMotorButton.isEnabled() && !this.tocarSomButton.isEnabled() && !this.alinharRodaButton.isEnabled()) {
             this.TextArea.setText(this.TextArea.getText() + "Carro com roda alinhada, motor ligado e som tocando" + "\n");
