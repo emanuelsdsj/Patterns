6f4d7fefb4a78cbdb014045e6dfef0951e4deeb2
Renato Novais <renatonovais@gmail.com>
2017-01-31 20:59:14 -0300

    Atualizado trabalho

---
+++ Trabalho INF011 Emanuel Sales/TetoSolar/src/tetosolar/TetoSolar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetosolar;
+
+import interfaces.IPrototype;
+
+/**
+ *
+ * @author aluno
+ */
+public class TetoSolar implements IPrototype {
+
+    @Override
+    public IPrototype clone() {
+        return new TetoSolar();
+    }
+
+    public String exibirInfo() {
+        return "Possui Teto Solar";
+    }
+    
+}
