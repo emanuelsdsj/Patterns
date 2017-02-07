1e579342e9552776fc67f56804146cbb47b33008
emanuelssj <emanuelolz@yahoo.com.br>
2017-02-07 19:10:51 -0300

    Corrigindo

---
+++ Estudando/src/videoGameXbox/ConsoleXbox.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoGameXbox;
+
+import Interfaces.IConsole;
+
+/**
+ *
+ * @author aluno
+ */
+public class ConsoleXbox implements IConsole {
+
+    @Override
+    public void Ligar() {
+        System.out.println("Ligando console do xbox");
+    }  
+}
